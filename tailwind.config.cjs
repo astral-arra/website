3d898dab4d26008aa2a3ad951796b309bad366c0
Petar Dzhambazov <jamby77@gmail.com>
2023-09-01 15:42:10 +0300

    basic pages added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('/images/oracle-girl.jpg')"
+      },
+      colors: {
+        'cloud-burst': {
+          '50': '#f1f5fd',
+          '100': '#dfe9fa',
+          '200': '#c6d9f7',
+          '300': '#9fc0f1',
+          '400': '#729fe8',
+          '500': '#517de0',
+          '600': '#3c61d4',
+          '700': '#334ec2',
+          '800': '#2f419e',
+          '900': '#2a3a7e',
+          '950': '#202852',
+        },
+      }
+    },
   },
   plugins: [],
 };
